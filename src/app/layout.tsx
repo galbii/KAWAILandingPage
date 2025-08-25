865515295588cf54b5a0d0105297bb47454f4e61
galbii <galbii@proton.me>
2025-08-25 14:30:36 -0700

    helloo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -55,6 +55,26 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 })(window,document,'script','dataLayer','GTM-NN27CTQX');`
         }} />
         {/* End Google Tag Manager */}
+        
+        {/* Meta Pixel Code */}
+        <script dangerouslySetInnerHTML={{
+          __html: `!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '783258114117252');
+fbq('track', 'PageView');`
+        }} />
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=783258114117252&ev=PageView&noscript=1"
+/>`
+        }} />
+        {/* End Meta Pixel Code */}
       </head>
       <body
         className={`${playfairDisplay.variable} ${inter.variable} antialiased`}
@@ -79,27 +99,6 @@ height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></nos
             gtag('config', 'AW-755074614');
           `}
         </Script>
-        
-        {/* Meta Pixel */}
-        <Script id="meta-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '783258114117252');
-            fbq('track', 'PageView');
-          `}
-        </Script>
-        <noscript>
-          {/* eslint-disable-next-line @next/next/no-img-element */}
-          <img height="1" width="1" style={{display: 'none'}} alt=""
-               src="https://www.facebook.com/tr?id=783258114117252&ev=PageView&noscript=1" />
-        </noscript>
       </body>
       <GoogleAnalytics gaId="G-P91EKWK0XB" />
     </html>
